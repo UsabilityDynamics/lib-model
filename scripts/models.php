f1bf1dc870e87dac17b4f67958e01b56d33e8151
Andy Potanin <andy.potanin@usabilitydynamics.com>
2013-09-08 10:27:42 -0400

    WIP

---
+++ scripts/models.php
@@ -0,0 +1,42 @@
+<?php
+  /**
+   * Model Handler
+   *
+   * @namespace UsabilityDynamics
+   * @module UsabilityDynamics
+   * @author potanin@UD
+   * @version 0.0.1
+   */
+  namespace UsabilityDynamics {
+
+    /**
+     * Class Models
+     *
+     *
+     * @author team@UD
+     * @version 0.1.1
+     * @class Models
+     * @subpackage Models
+     */
+    class Models {
+
+      // Class Version.
+      public $version = '0.1.1';
+
+      /**
+       * Constructor for the UD Models class.
+       *
+       * @author potanin@UD
+       * @version 0.0.1
+       * @method __construct
+       *
+       * @constructor
+       * @for Models
+       *
+       * @param array|bool $options
+       */
+      public function __construct( $options = false ) {}
+
+    }
+
+  }
