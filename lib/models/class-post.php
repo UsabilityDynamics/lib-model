67f8ee16b3f864e794f947450915a63e40dfe9f9
Maxim Peshkov <maxim.peshkov@usabilitydynamics.com>
2014-07-07 20:09:33 +0300

    post model (wip)

--- lib/models/class-post.php
+++ lib/models/class-post.php
@@ -30,6 +30,53 @@ namespace UsabilityDynamics\Model {
         }
         
       }
+      
+      /**
+       * Returns structure
+       *
+       * @param string $type
+       * @return array
+       */
+      public function getStructure( $type = null ) {
+        $structure = $this->_structure ? $this->_structure : array();
+        $structure = wp_parse_args( $structure, array(
+          'meta' => array(),
+          'terms' => array(),
+        ) );
+        if( !empty( $type ) ) {
+          if( key_exists( $type, $structure ) ) {
+            $structure = $structure[ $type ];
+          } else {
+           return false;
+          }
+        }
+        return $structure;
+      }
+      
+      /**
+       * Returns all meta data
+       *
+       * @return array
+       */
+      public function getMeta() {
+        $metas = $this->getStructure( 'meta' );
+        
+        $data = array();
+        foreach( $metas as $meta ) {
+          $data[ $meta ] = $this->meta->{$meta};
+        }
+        return $data;
+      }
+      
+      /**
+       * Returns all terms data
+       *
+       * @return array The list of WPModel\Term objects
+       */
+      public function getTerms() {
+        $terms = $this->getStructure( 'terms' );
+        return $this->terms( array( 'taxonomy' => $terms ) );
+      }
     
     }
   
